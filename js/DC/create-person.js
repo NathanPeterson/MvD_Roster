240aa05973d03c468249db3c280ea3896c87de2e
kcabico <cabico.k95@gmail.com>
2016-06-10 01:48:35 -0400

    Separate Marvel and DC

---
+++ js/DC/create-person.js
@@ -0,0 +1,29 @@
+$('#dc_form').on('submit', createPerson);
+
+function createPerson(ev){
+  ev.preventDefault();
+  var name = ev.currentTarget.name.value;
+  $.post({
+    url: peopleUrl,
+    data: {
+      person: {
+        name: name,
+        promoted: false,
+      }
+    },
+    success: addPerson
+  });
+}
+
+
+function addPerson(person){
+  var li = $('li.people-template')
+      .clone()
+      .removeClass('people-template')
+      .attr('data-id', person.person.id);
+
+  li.find('.person-name')
+    .html(person.person.name);
+
+  $('#Rivals').append(li);
+}
--- js/Marvel/app.js
+++
@@ -1,2 +0,0 @@
-var peopleUrl = 'https://mutant-school.herokuapp.com/api/v1/mutants'//'https://xtern-roster.herokuapp.com/people'
-var rivalUrl = 'https://xtern-roster.herokuapp.com/rivalries'
