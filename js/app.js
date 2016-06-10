139e3b83ea2c702b743e63d3f3db5ed1a6d14091
NathanPeterson <nalexpeterson@gmail.com>
2016-06-10 01:08:01 -0400

    Styling + Promote Functionality

--- js/app.js
+++ js/app.js
@@ -1,2 +1,2 @@
-var peopleUrl = 'https://xtern-roster.herokuapp.com/people'
+var peopleUrl = 'https://mutant-school.herokuapp.com/api/v1/mutants'//'https://xtern-roster.herokuapp.com/people'
 var rivalUrl = 'https://xtern-roster.herokuapp.com/rivalries'
--- js/create-person.js
+++ js/create-person.js
@@ -20,7 +20,7 @@ function addPerson(person){
   var li = $('li.people-template')
       .clone()
       .removeClass('people-template')
-      .attr('data-id', person.id);
+      .attr('data-id', person.person.id);
 
   li.find('.person-name')
     .html(person.person.name);
